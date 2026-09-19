d5b2648927517c5f7a1894d42bc51305c8b6518c
Do-nny <DonovanNelson05@gmail.com>
2026-09-19 10:59:55 +0200

    Remove duplicates

--- src/main/java/spl/parser/ProgramParser.java
+++ src/main/java/spl/parser/ProgramParser.java
@@ -3,7 +3,7 @@ import spl.lexer.TokenType;
 import spl.tree.Node;
 
 public class ProgramParser {
-    
+
     public static Node parseSplProg(ParserContext ctx){
         Node node = ctx.newNode("SPL_PROG");
         node.addChild(parseP(ctx));
@@ -101,14 +101,5 @@ public class ProgramParser {
         }
         return node;
     }
-
-    public static Node parseOutp(ParserContext ctx) {
-        return null;
-    }
-
-    public static Node parseAlgo(ParserContext ctx) {
-        throw new UnsupportedOperationException(
-                "ProgramParser.parseAlgo(ParserContext) has not been implemented yet"
-        );
-    }
 }
+
